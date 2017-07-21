5a9397d53fb448457023ae348fb51c7b7e02f419
Daniel Morales <dnlmrls9@gmail.com>
2017-07-21 18:00:51 +0200

    sidebar abbellimento

--- public/js/sidebar.controller.js
+++ public/js/sidebar.controller.js
@@ -10,17 +10,19 @@ app.controller('SideBarController', ['$scope', function($scope) {
     var ok = true;
 
     $scope.$on('tweet', function(event, data) {
-        if(ok) {
-            console.log(data);
-            ok = false;
-        }
+
+        var d = new Date(data.created_at);
+        var dateTime = d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
         var newEle = angular.element("" +
             "<div class='tweet'>" +
             "<div class='tweetHeader'>" +
             "<div class='tweetUser'>" +
             "<span><img class='tweetAvatar' src='"+data.user.profile_image_url+"'></span>" +
-            "<span><b>"+data.user.name+"</b></span></div>"
-            +"<div class='tweetBody'><p>"+ data.text+ "</p></div></div></div>");
+            "<span><b>"+data.user.name+"</b></span></div>"+
+            "<div class='tweetBody'><p>"+ data.text+ "</p></div>" +
+            "<div class='tweetFooter'><i class='fa fa-map-marker' aria-hidden='true'>&nbsp;"+data.place.full_name+"</i>&nbsp;&nbsp" +
+            "<i class='fa fa-clock-o' aria-hidden='true'></i>&nbsp;"+ dateTime +
+            "</div></div></div>");
         angular.element(target).prepend(newEle);
         if (tweets_number === MAX_TWEETS) {
             target.removeChild(target.lastChild);
