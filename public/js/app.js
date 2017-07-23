304b61296025926c7c60a7321171b20e37084f4d
Cristiano8 <cristiano.coluzza@gmail.com>
2017-07-23 19:45:55 +0200

    routes aggiunte

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,17 @@
 /**
  * Created by daniel on 20/07/17.
  */
-var app = angular.module('tweetviz', ['ngAnimate']);
+var app = angular.module('tweetviz', ['ngAnimate' , 'ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/map", {
+            templateUrl : "htm/map.htm"
+        })
+        .when("/chart", {
+            templateUrl : "htm/chart.htm"
+        })
+        .otherwise({
+            redirectTo : "htm/map.htm"
+        });
+});
